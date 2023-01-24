52de0f636ccb689097c55add240e9d0709623479
JeffJenison <jeff@quillian.io>
2023-01-23 19:28:23 -0500

    Create chapter_2.sql

---
+++ notebooks/chapter_2.sql
@@ -0,0 +1,17 @@
+/* *********************************
+
+LEARNING NOTEBOOK
+Learning SQL by Alan Beaulieu
+
+Jeffrey Jenison
+Started on: 2023-01-23
+
+********************************* */
+
+/* CHAPTER 2*/
+
+-- List tables in Sakila DB
+show tables;
+
+-- Describe Customer Table
+desc customer;
